9a0c7491efb7b11e025ec1f98d32ee82ad3a9a90
M.L.R <sun.ios06444@gmail.com>
2018-01-16 22:54:48 +0800

    Initial Commit

---
+++ 02_Hello_MLR_SUN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  02_Hello_MLR_SUN
+//
+//  Created by MBPr13 on 2018/1/16.
+//  Copyright © 2018年 MLR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
